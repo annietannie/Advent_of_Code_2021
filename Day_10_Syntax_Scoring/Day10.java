4b9ae194bfa66fab40abbf6868e51b6fe154995b
Anneke Kraamer <sogyo@HP400PD21.sogyo.nl>
2021-12-10 10:19:38 +0100

    Day 10.2

--- Day_10_Syntax_Scoring/Day10.java
+++ Day_10_Syntax_Scoring/Day10.java
@@ -4,6 +4,7 @@ import java.util.stream.IntStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.io.IOException;
 import java.lang.reflect.Array;
@@ -12,12 +13,20 @@ import java.util.List;
 
 public class Day10 {
     ArrayList<char[]> report = new ArrayList<>();
-    int score;
+    ArrayList<Long> scoreListPart2 = new ArrayList<>();
+    int scorePart1;
+    long scorePart2;
 
     public Day10(String fileName) {
-        this.score = 0;
+        this.scorePart1 = 0;
+        this.scorePart2 = 0;
         importFile(fileName);
-        findCorruptedLines();
+        scanReport();
+        //getReport();
+        calculateIncompletionPoints();
+ 
+        System.out.println("Score for corrupted lines: " + this.scorePart1);
+        System.out.println("Score for incomplete lines: " + this.scorePart2);
     }
 
     public void importFile(String fileName) {
@@ -33,11 +42,15 @@ public class Day10 {
         }
     } 
 
-    public void findCorruptedLines() {
-        for (char[] line : report) {
-            ArrayList<Character> chunks = new ArrayList<>();
+    public void scanReport() {
+        ArrayList<Character> chunks = new ArrayList<>();
+        for (int i=0; i<report.size(); i++) {
+            if (chunks.size() > 0) {
+                scoreListPart2.add(setPointsForIncompletion(chunks));
+            }
+            chunks.clear();
             lineloop:
-            for (char chunk : line) {
+            for (char chunk : report.get(i)) {
                 if (chunk == '(' || chunk == '[' || chunk == '{' || chunk == '<') {
                     chunks.add(chunk);
                 }
@@ -51,26 +64,68 @@ public class Day10 {
                     ) {
                         chunks.remove(chunks.size()-1);
                     } else {
-                        switch(chunk) {
-                            case ')':
-                                this.score += 3;
-                                break;
-                            case ']':
-                                this.score += 57;
-                                break;
-                            case '}':
-                                this.score += 1197;
-                                break;
-                            case '>':
-                                this.score += 25137;
-                        }
+                        setPointsForCorruption(chunk);
+                        chunks.clear();
+                        report.remove(i);
+                        i--;
                         break lineloop;
                     }
                 }
-
+                
             }
         }
-        System.out.println("Score for part 1: " + this.score);
+        scoreListPart2.add(setPointsForIncompletion(chunks));  
+    }
+
+    public void calculateIncompletionPoints() {
+        Collections.sort(scoreListPart2);
+        //System.out.println("Incompletion scores: " + scoreListPart2.toString());
+        this.scorePart2 = scoreListPart2.get(scoreListPart2.size()/2);
+    }
+
+    public long setPointsForIncompletion(ArrayList<Character> chunks) {
+        long scoringPart2 = 0;
+        for (int i=chunks.size()-1 ; i>=0; i--) {
+            char chunk = chunks.get(i);
+            scoringPart2 *= 5;
+            switch(chunk) {
+                case '(':
+                    scoringPart2 += 1;
+                    break;
+                case '[':
+                    scoringPart2 += 2;
+                    break;
+                case '{':
+                    scoringPart2 += 3;
+                    break;
+                case '<':
+                    scoringPart2 += 4;
+                    break;
+            }
+        }
+        return scoringPart2;
+    }
+
+    public void setPointsForCorruption(char chunk) {
+        switch(chunk) {
+            case ')':
+                this.scorePart1 += 3;
+                break;
+            case ']':
+                this.scorePart1 += 57;
+                break;
+            case '}':
+                this.scorePart1 += 1197;
+                break;
+            case '>':
+                this.scorePart1 += 25137;
+        }
+    }
+
+    public void getReport() {
+        for (char[] chunks : report) {
+            System.out.println(Arrays.toString(chunks));
+        }
     }
 
     public static void main (String[] args){
